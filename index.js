214dc44e6223f8504279c888a68ba53d543434e9
Sumei Kom <sumeikom@gmail.com>
2021-05-24 22:31:33 -0400

    commit tree 4bdacfa0b1cb5565dd7c944341a6aa6b1a1c9426

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceTravelledInFeet(start, end) {
+    return (start < end) ? (end - start) * 264 : (start - end) * 264;
+  }
+  
+  function distanceFromHqInBlocks(block) {
+    return (block > 42) ? block - 42 : 42 - block;
+  }
+  
+  function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264
+  }
+  
+  function calculatesFarePrice(start, end) {
+    const distance = distanceTravelledInFeet(start, end);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
